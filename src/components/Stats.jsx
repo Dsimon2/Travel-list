8ccee1e4709e15dfa97cd061830cfed0c4460808
Dsimon2 <browsedavid@gmail.com>
2023-07-27 18:59:36 -0400

    first commit

---
+++ src/components/Stats.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Stats({ items }) {
+  const numItems = items.length;
+
+  const numPacked = items.filter((item) => item.packed).length;
+
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      {/* if percentage is equals 100 render you have everything packed. You're ready to go. */}
+      {percentage === 100 ? (
+        "You have packed everything, You're ready to go!✈"
+      ) : (
+        <em>
+          You have {numItems} items on your list. and you have already packed{" "}
+          {numPacked} ({percentage}%)
+        </em>
+      )}
+    </footer>
+  );
+}
+
+export default Stats;
